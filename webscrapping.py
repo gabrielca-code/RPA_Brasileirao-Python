0c8f32a34f6c45cabbe07b5d485e2a314b222c6e
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2024-08-24 17:04:46 -0300

    Trocando a forma de armazenamento da extração, alimentando diretamente um dicionario

--- webscrapping.py
+++ webscrapping.py
@@ -15,45 +15,38 @@ linhas = soup.find_all('tr')
 linhas.pop(0)
 
 # Pegando as informações e passando para list
-posicoes = list(map(lambda time: time.select('.position')[0].text, linhas))
-times = list(map(lambda time: time.select('.team-name')[0].text, linhas))
-pontuacao = list(map(lambda time: time.select('.points')[0].text, linhas))
-qntd_jogos = list(map(lambda time: time.select('td[title="Jogos"]')[0].text, linhas))
-qtd_vitorias = list(map(lambda time: time.select('td[title="Vitórias"]')[0].text, linhas))
-qtd_empates = list(map(lambda time: time.select('td[title="Empates"]')[0].text, linhas))
-qtd_derrotas = list(map(lambda time: time.select('td[title="Derrotas"]')[0].text, linhas))
-gols = list(map(lambda time: time.select('td[title="Gols Pró"]')[0].text, linhas))
-gols_contra = list(map(lambda time: time.select('td[title="Gols Contra"]')[0].text, linhas))
+
+dados_classificacao = list(map(lambda time: {
+    'Posição': time.select('.position')[0].text,
+    'Time': time.select('.team-name')[0].text,
+    'Pontuação': time.select('.points')[0].text,
+    'Jogos': time.select('td[title="Jogos"]')[0].text,
+    'Vitórias': time.select('td[title="Vitórias"]')[0].text,
+    'Empates': time.select('td[title="Empates"]')[0].text,
+    'Derrotas': time.select('td[title="Derrotas"]')[0].text,
+    'Gols pró': time.select('td[title="Gols Pró"]')[0].text,
+    'Gols contra': time.select('td[title="Gols Contra"]')[0].text
+}, linhas))
 
 # Transformando as listas em um dataFrame 
-df = pd.DataFrame({
-    'Posição': posicoes,
-    'Time': times,
-    'Pontuação': pontuacao,
-    'Jogos': qntd_jogos,
-    'Vitórias': qtd_vitorias,
-    'Empates': qtd_empates,
-    'Derrotas': qtd_derrotas,
-    'Gols Pró': gols,
-    'Gols Contra': gols_contra
-})
+df = pd.DataFrame(dados_classificacao)
 
 # Fazendo as transformações
 df['Time'] = df['Time'].str.replace(' >>\n', '', regex=False)
 df['Posição'] = df['Posição'].map(int)
-df['Gols Pró'] = df['Gols Pró'].map(int)
-df['Gols Contra'] = df['Gols Contra'].map(int)
-df['Saldo Gols'] = df['Gols Pró'].map(int) - df['Gols Contra'].map(int)
+df['Gols pró'] = df['Gols pró'].map(int)
+df['Gols contra'] = df['Gols contra'].map(int)
+df['Saldo gols'] = df['Gols pró'].map(int) - df['Gols contra'].map(int)
 df['Jogos'] = df['Jogos'].map(int)
 df['Vitórias'] = df['Vitórias'].map(int)
 df['Empates'] = df['Empates'].map(int)
 df['Derrotas'] = df['Derrotas'].map(int)
 df['Aproveitamento'] = (((df['Vitórias'] * 3) + df['Empates']) / (df['Jogos'] * 3) * 100).round(2)
 
-df = df.sort_values(by='Gols Pró', ascending=False).reset_index(drop=True)
+df = df.sort_values(by='Gols pró', ascending=False).reset_index(drop=True)
 df['Ranking ataque'] = df.index + 1
 
-df = df.sort_values(by='Gols Contra', ascending=True).reset_index(drop=True)
+df = df.sort_values(by='Gols contra', ascending=True).reset_index(drop=True)
 df['Ranking defesa'] = df.index + 1
 
 df = df.sort_values(by='Posição').reset_index(drop=True)
@@ -63,9 +56,5 @@ def resumo_time(nomeTime):
     time = df[df['Time'].isin([nomeTime])].iloc[0].to_dict()
     return f"""{time['Time']}
 {time['Posição']} º - {time['Pontuação']} pontos em {time['Jogos']} jogos ({time['Vitórias']}V/{time['Empates']}E/{time['Derrotas']}D) - {time['Aproveitamento']} % de aproveitamento
-Gols pró: {time['Gols Pró']} - Gols contra: {time['Gols Contra']} - Saldo de gols: {time['Saldo Gols']}
+Gols pró: {time['Gols pró']} - Gols contra: {time['Gols contra']} - Saldo de gols: {time['Saldo gols']}
 Ranking ataque: {time['Ranking ataque']} - Ranking defesa: {time['Ranking defesa']}"""
-
-resumo_time('Cruzeiro')
-
-teste
