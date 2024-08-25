9fe6e05ab1f9af29ba64af3e178a710473d0329d
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2024-08-25 09:26:17 -0300

    Pegando todos os dados via site da gazeta esportiva

--- webscrapping.py
+++ webscrapping.py
@@ -6,7 +6,7 @@ url = 'https://www.terra.com.br/esportes/futebol/brasileiro-serie-a/tabela/'
 url_gazeta = 'https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/'
 
 # Requisição da página da Terra.com.br com a tabela do campeonato
-pagina = requests.get(url)
+pagina = requests.get(url_gazeta)
 
 # Convertendo em HTML o conteúdo
 soup = BeautifulSoup(pagina.content, 'html.parser')
@@ -18,24 +18,22 @@ classificacao = soup.find_all('tr')
 classificacao.pop(0)
 
 # Pegando as informações e passando para list
-
 dados_classificacao = list(map(lambda time: {
-    'Posição': time.select('.position')[0].text,
-    'Time': time.select('.team-name')[0].text,
-    'Pontuação': time.select('.points')[0].text,
-    'Jogos': time.select('td[title="Jogos"]')[0].text,
-    'Vitórias': time.select('td[title="Vitórias"]')[0].text,
-    'Empates': time.select('td[title="Empates"]')[0].text,
-    'Derrotas': time.select('td[title="Derrotas"]')[0].text,
-    'Gols pró': time.select('td[title="Gols Pró"]')[0].text,
-    'Gols contra': time.select('td[title="Gols Contra"]')[0].text
+    'Posição': time.select('.table__position')[0].text,
+    'Time': time.select('.team-link')[0]['title'],
+    'Pontuação': time.select('.table__stats')[0].text,
+    'Jogos': time.select('.table__stats')[1].text,
+    'Vitórias': time.select('.table__stats')[2].text,
+    'Empates': time.select('.table__stats')[3].text,
+    'Derrotas': time.select('.table__stats')[4].text,
+    'Gols pró': time.select('.table__stats')[5].text,
+    'Gols contra': time.select('.table__stats')[6].text,
 }, classificacao))
 
 # Transformando as listas em um dataFrame 
 df = pd.DataFrame(dados_classificacao)
 
 # Fazendo as transformações necessárias
-df['Time'] = df['Time'].str.replace(' >>\n', '', regex=False)
 df['Posição'] = df['Posição'].map(int)
 df['Gols pró'] = df['Gols pró'].map(int)
 df['Gols contra'] = df['Gols contra'].map(int)
@@ -71,14 +69,28 @@ soup = BeautifulSoup(pagina.content, 'html.parser')
 # Extraindo todos os jogos
 jogos = soup.find_all('li', class_='table__games__item')
 
+#print(list(jogos)[0].select('span')[5].text)
+
 dados_jogos = list(map(lambda jogo: {
-    'Mandante': jogo.select('a')[0].text,
+    'Mandante': jogo.select('a')[0]['title'],
     'Placar mandante': jogo.select('span')[5].text,
-    'Visitante': jogo.select('a')[4].text,
-    'Placar visitante': jogo.select('span')[7].text,
+    'Visitante': jogo.select('a')[3]['title'],
+    'Placar visitante': jogo.select('span')[6].text,
     'Data': jogo.select('span')[0].text
 }, jogos))
 
-df = pd.DataFrame(dados_jogos)
+df2 = pd.DataFrame(dados_jogos)
 
-print(df)
+df2['Placar mandante'] = df2['Placar mandante'].apply(lambda cell: int(cell) if pd.notna(cell) and cell.strip() != '' and cell.isdigit() else cell)
+df2['Placar visitante'] = df2['Placar visitante'].apply(lambda cell: int(cell) if pd.notna(cell) and cell.strip() != '' and cell.isdigit() else cell)
+df2['Data'] = df2['Data'].apply(lambda cell: cell.split('\n')[1] if pd.notna(cell) and cell.strip() != '' and len(cell.split('\n')) > 1 else cell)
+df2['Data'] = pd.to_datetime(df2['Data'], format='%d/%m %H:%M', errors='coerce')
+df2['Data'] = df2['Data'].apply(lambda dt: dt.replace(year=2024) if pd.notna(dt) else dt)
+
+df2['Resultado'] = df2.apply(
+    lambda row: 'Jogo não realizado' if row['Placar mandante'] == ''
+                else 'Visitante' if row['Placar mandante'] < row['Placar visitante']  
+                else 'Mandante' if row['Placar mandante'] > row['Placar visitante']
+                else 'Empate',
+    axis=1
+)
