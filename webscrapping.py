aa0af3840e23efab288a682cd53c794c88a2e13a
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2024-08-26 22:07:32 -0300

    Adicionando parâmetros de gols feitos nos ultimos jogos geral, mandante e visitante

--- webscrapping.py
+++ webscrapping.py
@@ -6,6 +6,7 @@ url_gazeta = 'https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/'
 
 # Requisição da página da Terra.com.br com a tabela do campeonato
 pagina = requests.get(url_gazeta)
+print(pagina)
 
 # Convertendo em HTML o conteúdo
 soup = BeautifulSoup(pagina.content, 'html.parser')
@@ -23,6 +24,7 @@ dados_jogos = list(map(lambda jogo: {
 }, jogos))
 
 df2 = pd.DataFrame(dados_jogos)
+print(df2)
 df2['Placar mandante'] = df2['Placar mandante'].apply(lambda cell: int(cell) if pd.notna(cell) and cell.strip() != '' and cell.isdigit() else cell)
 df2['Placar visitante'] = df2['Placar visitante'].apply(lambda cell: int(cell) if pd.notna(cell) and cell.strip() != '' and cell.isdigit() else cell)
 df2['Data'] = df2['Data'].apply(lambda cell: cell.split('\n')[1] if pd.notna(cell) and cell.strip() != '' and len(cell.split('\n')) > 1 else cell)
@@ -80,11 +82,13 @@ df = df.sort_values(by='Posição').reset_index(drop=True)
 df['Jogos mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Mandante'] == time) & (df2['Resultado'] != 'Jogo não realizado')]))
 df['Vitórias mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Mandante') & (df2['Mandante'] == time)]))
 df['Empates mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Empate') & (df2['Mandante'] == time)]))
+df['Derrotas mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Visitante') & (df2['Mandante'] == time)]))
 df['Aproveitamento mandante'] = ((df['Vitórias mandante'] * 3 + df['Empates mandante']) / (df['Jogos mandante'] * 3) * 100).round(2)
 
 df['Jogos visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Visitante'] == time) & (df2['Resultado'] != 'Jogo não realizado')]))
 df['Vitórias visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Visitante') & (df2['Visitante'] == time)]))
 df['Empates visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Empate') & (df2['Visitante'] == time)]))
+df['Derrotas visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Mandante') & (df2['Visitante'] == time)]))
 df['Aproveitamento visitante'] = ((df['Vitórias visitante'] * 3 + df['Empates visitante']) / (df['Jogos visitante'] * 3) * 100).round(2)
 
 def ultimos_jogos(time):
@@ -94,7 +98,6 @@ def ultimos_jogos(time):
     i = 5
 
     for i in range(quantidade_ultimos_jogos, 0, -1):
-        print(i)
         resultado = ultimos_jogos.iloc[i-1]['Resultado']
         mandante_visitante = 'Mandante' if ultimos_jogos.iloc[i-1]['Mandante'] == time else 'Visitante'        
         string_ultimos_jogos = string_ultimos_jogos + ('E' if resultado == 'Empate' else 'V' if resultado == mandante_visitante else 'D')
@@ -108,7 +111,6 @@ def ultimos_jogos_mandante(time):
     i = 5
 
     for i in range(quantidade_ultimos_jogos, 0, -1):
-        print(i)
         resultado = ultimos_jogos.iloc[i-1]['Resultado']
         string_ultimos_jogos = string_ultimos_jogos + ('E' if resultado == 'Empate' else 'V' if resultado == 'Mandante' else 'D')
 
@@ -121,22 +123,76 @@ def ultimos_jogos_visitante(time):
     i = 5
 
     for i in range(quantidade_ultimos_jogos, 0, -1):
-        print(i)
         resultado = ultimos_jogos.iloc[i-1]['Resultado']
         string_ultimos_jogos = string_ultimos_jogos + ('E' if resultado == 'Empate' else 'V' if resultado == 'Visitante' else 'D')
 
     return string_ultimos_jogos
 
+def ultimos_jogos_gols(time):
+    ultimos_jogos = df2[(df2['Resultado'] != 'Jogo não realizado') & ((df2['Mandante'] == time) | (df2['Visitante'] == time))]
+    quantidade_gols = 0
+    quantidade_ultimos_jogos = 5 if len(ultimos_jogos) >= 5 else len(ultimos_jogos)
+    i = 0
+
+    for i in range(quantidade_ultimos_jogos):
+        quantidade_gols = quantidade_gols + (ultimos_jogos.iloc[i]['Placar mandante'] if ultimos_jogos.iloc[i]['Mandante'] == time else ultimos_jogos.iloc[i]['Placar visitante'])
+
+    return quantidade_gols
+
+def ultimos_jogos_gols_mandante(time):
+    ultimos_jogos = df2[(df2['Resultado'] != 'Jogo não realizado') & ((df2['Mandante'] == time))]
+    quantidade_gols = 0
+    quantidade_ultimos_jogos = 5 if len(ultimos_jogos) >= 5 else len(ultimos_jogos)
+    i = 0
+
+    for i in range(quantidade_ultimos_jogos):
+        quantidade_gols = quantidade_gols + ultimos_jogos.iloc[i]['Placar mandante']
+
+    return quantidade_gols
+
+def ultimos_jogos_gols_visitante(time):
+    ultimos_jogos = df2[(df2['Resultado'] != 'Jogo não realizado') & ((df2['Visitante'] == time))]
+    quantidade_gols = 0
+    quantidade_ultimos_jogos = 5 if len(ultimos_jogos) >= 5 else len(ultimos_jogos)
+    i = 0
+
+    for i in range(quantidade_ultimos_jogos):
+        quantidade_gols = quantidade_gols + ultimos_jogos.iloc[i]['Placar visitante']
+
+    return quantidade_gols
+
 df['Ultimos jogos'] = df['Time'].apply(ultimos_jogos)
 df['Ultimos jogos - Mandante'] = df['Time'].apply(ultimos_jogos_mandante)
 df['Ultimos jogos - Visitante'] = df['Time'].apply(ultimos_jogos_visitante)
+
+df['Gols ultimos jogos'] = df['Time'].apply(ultimos_jogos_gols)
+df['Gols ultimos jogos - Mandante'] = df['Time'].apply(ultimos_jogos_gols_mandante)
+df['Gols ultimos jogos - Visitante'] = df['Time'].apply(ultimos_jogos_gols_visitante)
 
 # Trazendo as informações
 def resumo_time(nomeTime):
     time = df[df['Time'].isin([nomeTime])].iloc[0].to_dict()
     return f"""{time['Time']}
+Retrospecto geral:
 {time['Posição']} º - {time['Pontuação']} pontos em {time['Jogos']} jogos ({time['Vitórias']}V/{time['Empates']}E/{time['Derrotas']}D) - {time['Aproveitamento']} % de aproveitamento
 Gols pró: {time['Gols pró']} - Gols contra: {time['Gols contra']} - Saldo de gols: {time['Saldo gols']}
-Ranking ataque: {time['Ranking ataque']} - Ranking defesa: {time['Ranking defesa']}"""
+Ranking ataque: {time['Ranking ataque']} - Ranking defesa: {time['Ranking defesa']}
 
-print(df)
+Restrospecto últimos 5 jogos
+Últimos jogos: {time['Ultimos jogos']}
+Gols pró: {time['Gols ultimos jogos']}
+
+Retrospecto mandante:
+{time['Jogos mandante']} jogos ({time['Vitórias mandante']}V/{time['Empates mandante']}E/{time['Derrotas mandante']}D) - {time['Aproveitamento mandante']}
+Últimos jogos: {time['Ultimos jogos - Mandante']}
+Gols pró: {time['Gols ultimos jogos - Mandante']}
+
+Retrospecto visitante:
+{time['Jogos visitante']} jogos ({time['Vitórias visitante']}V/{time['Empates visitante']}E/{time['Derrotas visitante']}D) - {time['Aproveitamento visitante']}
+Últimos jogos: {time['Ultimos jogos - Visitante']}
+Gols pró: {time['Gols ultimos jogos - Visitante']}
+
+"""
+
+print(resumo_time('Fluminense'))
+print(resumo_time('São Paulo'))
