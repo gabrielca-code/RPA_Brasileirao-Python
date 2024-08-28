43812b3255f48c469063c85363876f1969825850
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2024-08-27 23:55:46 -0300

    Passando todo algoritmo para fórmulas

--- webscrapping.py
+++ webscrapping.py
@@ -4,92 +4,84 @@ from bs4 import BeautifulSoup
 
 url_gazeta = 'https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/'
 
-# Requisição da página da Terra.com.br com a tabela do campeonato
-pagina = requests.get(url_gazeta)
-print(pagina)
+def requisicaoPagina():
+    while True:
+        global pagina 
+        pagina = requests.get(url_gazeta)
+        if pagina.status_code == 200:
+            break
+        else:
+            print(pagina.status_code)
+    
+def converterRequisicaoParaHTML() :
+    global soup  
+    soup = BeautifulSoup(pagina.content, 'html.parser')
 
-# Convertendo em HTML o conteúdo
-soup = BeautifulSoup(pagina.content, 'html.parser')
+def extrairJogosHTML():
+    global jogos 
+    jogos = soup.find_all('li', class_='table__games__item')
 
-# Extração e transformação dos dados da classificação
-# Extraindo todos os jogos
-jogos = soup.find_all('li', class_='table__games__item')
+def criarDicionarioJogos():
+    global dados_jogos 
+    dados_jogos = list(map(lambda jogo: {
+        'Mandante': jogo.select('a')[0]['title'],
+        'Placar mandante': jogo.select('span')[5].text,
+        'Visitante': jogo.select('a')[3]['title'],
+        'Placar visitante': jogo.select('span')[6].text,
+        'Data': jogo.select('span')[0].text
+    }, jogos))
 
-dados_jogos = list(map(lambda jogo: {
-    'Mandante': jogo.select('a')[0]['title'],
-    'Placar mandante': jogo.select('span')[5].text,
-    'Visitante': jogo.select('a')[3]['title'],
-    'Placar visitante': jogo.select('span')[6].text,
-    'Data': jogo.select('span')[0].text
-}, jogos))
+def converterDicionarioEmDataFrame():
+    global df2
+    df2 = pd.DataFrame(dados_jogos)
 
-df2 = pd.DataFrame(dados_jogos)
-print(df2)
-df2['Placar mandante'] = df2['Placar mandante'].apply(lambda cell: int(cell) if pd.notna(cell) and cell.strip() != '' and cell.isdigit() else cell)
-df2['Placar visitante'] = df2['Placar visitante'].apply(lambda cell: int(cell) if pd.notna(cell) and cell.strip() != '' and cell.isdigit() else cell)
-df2['Data'] = df2['Data'].apply(lambda cell: cell.split('\n')[1] if pd.notna(cell) and cell.strip() != '' and len(cell.split('\n')) > 1 else cell)
-df2['Data'] = pd.to_datetime(df2['Data'], format='%d/%m %H:%M', errors='coerce')
-df2['Data'] = df2['Data'].apply(lambda dt: dt.replace(year=2024) if pd.notna(dt) else dt)
-df2['Resultado'] = df2.apply(
-    lambda row: 'Jogo não realizado' if row['Placar mandante'] == ''
-                else 'Visitante' if row['Placar mandante'] < row['Placar visitante']  
-                else 'Mandante' if row['Placar mandante'] > row['Placar visitante']
-                else 'Empate',
-    axis=1
-)
+def transformacoesDataFrameJogos():
+    df2['Placar mandante'] = df2['Placar mandante'].apply(lambda cell: int(cell) if pd.notna(cell) and cell.strip() != '' and cell.isdigit() else cell)
+    df2['Placar visitante'] = df2['Placar visitante'].apply(lambda cell: int(cell) if pd.notna(cell) and cell.strip() != '' and cell.isdigit() else cell)
+    df2['Data'] = df2['Data'].apply(lambda cell: cell.split('\n')[1] if pd.notna(cell) and cell.strip() != '' and len(cell.split('\n')) > 1 else cell)
+    df2['Data'] = pd.to_datetime(df2['Data'], format='%d/%m %H:%M', errors='coerce')
+    df2['Data'] = df2['Data'].apply(lambda dt: dt.replace(year=2024) if pd.notna(dt) else dt)
+    df2['Resultado'] = df2.apply(lambda row: 'Jogo não realizado' if row['Placar mandante'] == '' else 'Visitante' if row['Placar mandante'] < row['Placar visitante'] else 'Mandante' if row['Placar mandante'] > row['Placar visitante'] else 'Empate', axis=1)
+    df2 = df2.sort_values(by='Data', ascending=False)
 
-df2 = df2.sort_values(by='Data', ascending=False)
+def extrairClassificacaoHTML():
+    global classificacao
+    classificacao = soup.find_all('tr')
+    classificacao.pop(0)
 
-# Extração e transformação dos dados da classificação
-# Pegando todos elementos dentro das tags TR
-classificacao = soup.find_all('tr')
+def criarDicionarioClassificacao():
+    global dados_classificacao
+    dados_classificacao = list(map(lambda time: {
+        'Posição': time.select('.table__position')[0].text,
+        'Time': time.select('.team-link')[0]['title'],
+        'Pontuação': time.select('.table__stats')[0].text,
+        'Jogos': time.select('.table__stats')[1].text,
+        'Vitórias': time.select('.table__stats')[2].text,
+        'Empates': time.select('.table__stats')[3].text,
+        'Derrotas': time.select('.table__stats')[4].text,
+        'Gols pró': time.select('.table__stats')[5].text,
+        'Gols contra': time.select('.table__stats')[6].text,
+    }, classificacao))
 
-# Removendo a primeira linha (cabeçalho da tabela)
-classificacao.pop(0)
+def converterDicionarioEmDataFrame2():
+    global df 
+    df = pd.DataFrame(dados_classificacao)
 
-# Pegando as informações e passando para list
-dados_classificacao = list(map(lambda time: {
-    'Posição': time.select('.table__position')[0].text,
-    'Time': time.select('.team-link')[0]['title'],
-    'Pontuação': time.select('.table__stats')[0].text,
-    'Jogos': time.select('.table__stats')[1].text,
-    'Vitórias': time.select('.table__stats')[2].text,
-    'Empates': time.select('.table__stats')[3].text,
-    'Derrotas': time.select('.table__stats')[4].text,
-    'Gols pró': time.select('.table__stats')[5].text,
-    'Gols contra': time.select('.table__stats')[6].text,
-}, classificacao))
-
-# Transformando as listas em um dataFrame 
-df = pd.DataFrame(dados_classificacao)
-
-# Fazendo as transformações necessárias
-df['Posição'] = df['Posição'].map(int)
-df['Gols pró'] = df['Gols pró'].map(int)
-df['Gols contra'] = df['Gols contra'].map(int)
-df['Saldo gols'] = df['Gols pró'].map(int) - df['Gols contra'].map(int)
-df['Jogos'] = df['Jogos'].map(int)
-df['Vitórias'] = df['Vitórias'].map(int)
-df['Empates'] = df['Empates'].map(int)
-df['Derrotas'] = df['Derrotas'].map(int)
-df['Aproveitamento'] = (((df['Vitórias'] * 3) + df['Empates']) / (df['Jogos'] * 3) * 100).round(2)
-df = df.sort_values(by='Gols pró', ascending=False).reset_index(drop=True)
-df['Ranking ataque'] = df.index + 1
-df = df.sort_values(by='Gols contra', ascending=True).reset_index(drop=True)
-df['Ranking defesa'] = df.index + 1
-df = df.sort_values(by='Posição').reset_index(drop=True)
-
-df['Jogos mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Mandante'] == time) & (df2['Resultado'] != 'Jogo não realizado')]))
-df['Vitórias mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Mandante') & (df2['Mandante'] == time)]))
-df['Empates mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Empate') & (df2['Mandante'] == time)]))
-df['Derrotas mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Visitante') & (df2['Mandante'] == time)]))
-df['Aproveitamento mandante'] = ((df['Vitórias mandante'] * 3 + df['Empates mandante']) / (df['Jogos mandante'] * 3) * 100).round(2)
-
-df['Jogos visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Visitante'] == time) & (df2['Resultado'] != 'Jogo não realizado')]))
-df['Vitórias visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Visitante') & (df2['Visitante'] == time)]))
-df['Empates visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Empate') & (df2['Visitante'] == time)]))
-df['Derrotas visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Mandante') & (df2['Visitante'] == time)]))
-df['Aproveitamento visitante'] = ((df['Vitórias visitante'] * 3 + df['Empates visitante']) / (df['Jogos visitante'] * 3) * 100).round(2)
+def transformacoesDataFrameClassificacao():
+    df['Posição'] = df['Posição'].map(int)
+    df['Gols pró'] = df['Gols pró'].map(int)
+    df['Gols contra'] = df['Gols contra'].map(int)
+    df['Saldo gols'] = df['Gols pró'].map(int) - df['Gols contra'].map(int)
+    df['Jogos'] = df['Jogos'].map(int)
+    df['Vitórias'] = df['Vitórias'].map(int)
+    df['Empates'] = df['Empates'].map(int)
+    df['Derrotas'] = df['Derrotas'].map(int)
+    df['Aproveitamento'] = (((df['Vitórias'] * 3) + df['Empates']) / (df['Jogos'] * 3) * 100).round(2)
+    df = df.sort_values(by='Gols pró', ascending=False).reset_index(drop=True)
+    df['Ranking ataque'] = df.index + 1
+    df = df.sort_values(by='Gols contra', ascending=True).reset_index(drop=True)
+    df['Ranking defesa'] = df.index + 1
+    df = df.sort_values(by='Posição').reset_index(drop=True)
 
 def ultimos_jogos(time):
     ultimos_jogos = df2[(df2['Resultado'] != 'Jogo não realizado') & ((df2['Mandante'] == time) | (df2['Visitante'] == time))]
@@ -194,42 +186,82 @@ def ultimos_jogos_golsc_visitante(time):
 
     return quantidade_gols
 
-df['Ultimos jogos'] = df['Time'].apply(ultimos_jogos)
-df['Ultimos jogos - Mandante'] = df['Time'].apply(ultimos_jogos_mandante)
-df['Ultimos jogos - Visitante'] = df['Time'].apply(ultimos_jogos_visitante)
+def transformacoesInterDataFrames():
+    df['Jogos mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Mandante'] == time) & (df2['Resultado'] != 'Jogo não realizado')]))
+    df['Vitórias mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Mandante') & (df2['Mandante'] == time)]))
+    df['Empates mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Empate') & (df2['Mandante'] == time)]))
+    df['Derrotas mandante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Visitante') & (df2['Mandante'] == time)]))
+    df['Aproveitamento mandante'] = ((df['Vitórias mandante'] * 3 + df['Empates mandante']) / (df['Jogos mandante'] * 3) * 100).round(2)
 
-df['Gols ultimos jogos'] = df['Time'].apply(ultimos_jogos_gols)
-df['Gols ultimos jogos - Mandante'] = df['Time'].apply(ultimos_jogos_gols_mandante)
-df['Gols ultimos jogos - Visitante'] = df['Time'].apply(ultimos_jogos_gols_visitante)
+    df['Jogos visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Visitante'] == time) & (df2['Resultado'] != 'Jogo não realizado')]))
+    df['Vitórias visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Visitante') & (df2['Visitante'] == time)]))
+    df['Empates visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Empate') & (df2['Visitante'] == time)]))
+    df['Derrotas visitante'] = df['Time'].apply(lambda time: len(df2[(df2['Resultado'] == 'Mandante') & (df2['Visitante'] == time)]))
+    df['Aproveitamento visitante'] = ((df['Vitórias visitante'] * 3 + df['Empates visitante']) / (df['Jogos visitante'] * 3) * 100).round(2)
 
-df['Gols contra ultimos jogos'] = df['Time'].apply(ultimos_jogos_golsc)
-df['Gols contra ultimos jogos - Mandante'] = df['Time'].apply(ultimos_jogos_golsc_mandante)
-df['Gols contra ultimos jogos - Visitante'] = df['Time'].apply(ultimos_jogos_golsc_visitante)
+    df['Ultimos jogos'] = df['Time'].apply(ultimos_jogos)
+    df['Ultimos jogos - Mandante'] = df['Time'].apply(ultimos_jogos_mandante)
+    df['Ultimos jogos - Visitante'] = df['Time'].apply(ultimos_jogos_visitante)
 
-# Trazendo as informações
-def resumo_time(nomeTime):
-    time = df[df['Time'].isin([nomeTime])].iloc[0].to_dict()
+    df['Gols ultimos jogos'] = df['Time'].apply(ultimos_jogos_gols)
+    df['Gols ultimos jogos - Mandante'] = df['Time'].apply(ultimos_jogos_gols_mandante)
+    df['Gols ultimos jogos - Visitante'] = df['Time'].apply(ultimos_jogos_gols_visitante)
+
+    df['Gols contra ultimos jogos'] = df['Time'].apply(ultimos_jogos_golsc)
+    df['Gols contra ultimos jogos - Mandante'] = df['Time'].apply(ultimos_jogos_golsc_mandante)
+    df['Gols contra ultimos jogos - Visitante'] = df['Time'].apply(ultimos_jogos_golsc_visitante)
+
+def procurarTime(nomeTime):
+    return df[df['Time'] == nomeTime].iloc[0]
+
+def apresentarRetrospectoGeral(time):
     return f"""{time['Time']}
 Retrospecto geral:
 {time['Posição']} º - {time['Pontuação']} pontos em {time['Jogos']} jogos ({time['Vitórias']}V/{time['Empates']}E/{time['Derrotas']}D) - {time['Aproveitamento']} % de aproveitamento
 Gols pró: {time['Gols pró']} - Gols contra: {time['Gols contra']} - Saldo de gols: {time['Saldo gols']}
 Ranking ataque: {time['Ranking ataque']} - Ranking defesa: {time['Ranking defesa']}
+"""
 
-Restrospecto últimos 5 jogos
+def apresentarRetrospectoGeralUltimos5Jogos(time):
+    return f"""Restrospecto últimos 5 jogos
 Últimos jogos: {time['Ultimos jogos']}
 Gols pró: {time['Gols ultimos jogos']} - Gols contra: {time['Gols contra ultimos jogos']}
+"""
 
-Retrospecto mandante:
+def apresentarRetrospectoGeralUltimos5JogosMandante(time):
+    return f"""Retrospecto mandante:
 {time['Jogos mandante']} jogos ({time['Vitórias mandante']}V/{time['Empates mandante']}E/{time['Derrotas mandante']}D) - {time['Aproveitamento mandante']}
 Últimos jogos: {time['Ultimos jogos - Mandante']}
 Gols pró: {time['Gols ultimos jogos - Mandante']} - Gols contra: {time['Gols contra ultimos jogos - Mandante']}
+"""
 
-Retrospecto visitante:
+def apresentarRetrospectoGeralUltimos5JogosVisitante(time):
+    return f"""Retrospecto visitante:
 {time['Jogos visitante']} jogos ({time['Vitórias visitante']}V/{time['Empates visitante']}E/{time['Derrotas visitante']}D) - {time['Aproveitamento visitante']}
 Últimos jogos: {time['Ultimos jogos - Visitante']}
 Gols pró: {time['Gols ultimos jogos - Visitante']} - Gols contra: {time['Gols contra ultimos jogos - Visitante']}
-
 """
 
-print(resumo_time('Fluminense'))
-print(resumo_time('São Paulo'))
+def resumo_geral(time):
+    return apresentarRetrospectoGeral(time) + apresentarRetrospectoGeralUltimos5Jogos(time) + apresentarRetrospectoGeralUltimos5JogosMandante(time) + apresentarRetrospectoGeralUltimos5JogosVisitante(time)
+
+requisicaoPagina()
+if pagina.status_code == 200:
+    converterRequisicaoParaHTML()
+
+    extrairJogosHTML()
+    criarDicionarioJogos()
+    converterDicionarioEmDataFrame()
+    transformacoesDataFrameJogos()
+
+    extrairClassificacaoHTML()
+    criarDicionarioClassificacao()
+    converterDicionarioEmDataFrame2()
+    transformacoesDataFrameClassificacao()
+
+    transformacoesInterDataFrames()
+
+    print(resumo_geral(procurarTime('Fluminense')))
+    print(resumo_geral(procurarTime('São Paulo')))
+else:
+    print(f"Erro, código {pagina.status_code}")
